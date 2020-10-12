637901190f1dc59dcb1c027aa5f29a70adbc18a1
Carson McCaffrey <carson@Carsons-MacBook-Pro.local>
2020-10-12 17:06:11 -0400

    BS4 with Regex to clear HTML

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,24 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 import os, ssl
+import re
 
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
+
+def clear_attributes(text):
+    temp = text
+    temp = re.sub(r'<[^>]*>', '', temp, flags=re.MULTILINE)
+    return temp
 
 url = "https://www.cnn.com/2020/10/06/politics/trump-ends-stimulus-talks/index.html"
 
 page = urlopen(url)
 
-print(page)
+soup = BeautifulSoup(page.read(), features="lxml")
+
+#print(soup)
+
+for div in soup.find_all('div', attrs={'class': 'zn-body__paragraph'}):
+    print(clear_attributes(str(div)))
+
